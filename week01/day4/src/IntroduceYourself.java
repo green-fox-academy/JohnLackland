edad800a506f32d0e99a7a81891a9a10b5f131c9
JohnLacklan <janos.f96@gmail.com>
2019-01-17 13:04:43 +0100

    finishing exercise 4

--- week01/day4/src/IntroduceYourself.java
+++ week01/day4/src/IntroduceYourself.java
@@ -1,2 +1,7 @@
 public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Farkas Janos");
+        System.out.println(22);
+        System.out.println(1.81);
+    }
 }
