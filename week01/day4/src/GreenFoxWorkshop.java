1133ca78369d7b5dd847eae883eb574e6beae081
JohnLacklan <janos.f96@gmail.com>
2019-01-17 12:55:45 +0100

    make exercises 1,2,3.

--- week01/day4/src/GreenFoxWorkshop.java
+++ week01/day4/src/GreenFoxWorkshop.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class GreenFoxWorkshop {
+
+    public static void main(String[] args) {
+        int a = 55;
+        if (a % 10 == 5) {
+            System.out.println(a);
+            a += 15;
+            System.out.println(a);
+
+
+
+        }
+
+
+
+
+    }
 }
---
+++ week01/day4/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, Boti");
+        System.out.println("Hello, Aniko");
+        System.out.println("hello, Krisztian");
+    }
+}
