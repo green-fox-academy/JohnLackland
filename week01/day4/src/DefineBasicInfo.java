036b1b4e83fd24bf5debdb717c63554e0b47e514
JohnLacklan <janos.f96@gmail.com>
2019-01-17 15:26:59 +0100

    finishing some exercises

---
+++ week01/day4/src/DefineBasicInfo.java
@@ -0,0 +1,13 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+
+
+        String name = "Farkas Janos";
+
+        int age = 22;
+
+        double height = 1.81;
+
+        boolean married = false;
+    }
+}
---
+++ week01/day4/src/Swap.java
@@ -0,0 +1,12 @@
+public class Swap {
+    public static void main(String[] args) {
+        int a = 526;
+        int b = 123;
+
+
+
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
