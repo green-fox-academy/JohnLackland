724595e67c98240bcbb8fbc94060f08845ce344d
JohnLacklan <janos.f96@gmail.com>
2019-01-17 14:24:30 +0100

    finishing exersies 6-7

---
+++ week01/day4/src/CodingHours.java
@@ -0,0 +1,8 @@
+public class CodingHours {
+    public static void main(String[] args) {
+
+        System.out.println("Attandees spent " + 17*5*6 + " hours with coding.");
+
+        System.out.println("The percentage of the coding hours in the smester is " + ( (17*5*6.) / ( 17*52. ))* 100 );
+    }
+}
