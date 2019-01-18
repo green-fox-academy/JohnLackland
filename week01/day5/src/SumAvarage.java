dcfc1f0315938baeb68272e62063eba2ecb32953
JohnLacklan <janos.f96@gmail.com>
2019-01-18 16:06:01 +0100

    finishing some exercises

--- week01/day5/src/SumAvarage.java
+++ week01/day5/src/SumAvarage.java
@@ -1,6 +1,36 @@
+import java.util.Scanner;
 
 public class SumAvarage {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 0;
+        int summ = 0;
+        boolean win = true;
+
+        while (win){
+
+            int userInput = sc.nextInt();
+
+            summ += userInput;
+            a++;
+            System.out.println("Sum = " + summ + ", Avarage: " + summ / a);
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
 
     }
 }
