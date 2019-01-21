49b5d32a3c46c057177ee4347831f967441d6e14
JohnLacklan <janos.f96@gmail.com>
2019-01-21 16:00:24 +0100

    Make a two dimensonal array that contains colors

---
+++ week2/day1/src/Colors.java
@@ -0,0 +1,18 @@
+public class Colors {
+    public static void main(String[] args) {
+
+
+        String[][] colors = {{"lime","forest green","olive", "pale green", "spring green"},{"orange red","red","tomato"},{"orchid", "violet",   "pink", "hot pink"}};
+
+
+
+
+
+
+
+
+        //String[][] color = {"lime","forest green","olive", "pale green", "spring green"};
+        //String[] color1 = {"orange red","red","tomato"};
+        //String[] color2 = {"orchid", "violet", "pink", "hot pink"};
+    }
+}
