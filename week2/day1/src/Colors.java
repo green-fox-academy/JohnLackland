2e1b8c55658a59473b81b07d5cfbcac544cbb9fb
JohnLacklan <janos.f96@gmail.com>
2019-01-21 16:11:53 +0100

    Add a after the words

--- week2/day1/src/Colors.java
+++ week2/day1/src/Colors.java
@@ -4,6 +4,13 @@ public class Colors {
 
         String[][] colors = {{"lime","forest green","olive", "pale green", "spring green"},{"orange red","red","tomato"},{"orchid", "violet",   "pink", "hot pink"}};
 
+        for (int i = 0; i < colors.length; i++) {
+            for (int j = 0; j < colors[i].length; j++) {
+                System.out.print(colors[i][j]+ " | ");
+            }
+            System.out.println();
+        }
+
 
 
 
