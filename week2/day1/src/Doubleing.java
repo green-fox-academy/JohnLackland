49c83b3fbe62587a6a9fd983711e9440dc96658b
JohnLacklan <janos.f96@gmail.com>
2019-01-21 13:56:06 +0100

    double a number with function

---
+++ week2/day1/src/Doubleing.java
@@ -0,0 +1,18 @@
+public class Doubleing {
+    public static void main(String[] args) {
+        int baseNum = 123;
+
+
+        System.out.println(Doubling(baseNum));
+    }
+
+    public static int Doubling(int baseNum  ){
+
+        int a = baseNum * 2;
+
+        return a;
+
+
+
+    }
+}
