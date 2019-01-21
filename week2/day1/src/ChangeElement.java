b33a4d6caf655e2389fa2458504034043552970c
JohnLacklan <janos.f96@gmail.com>
2019-01-21 15:05:18 +0100

    change an element of a array

---
+++ week2/day1/src/ChangeElement.java
@@ -0,0 +1,12 @@
+public class ChangeElement {
+    public static void main(String[] args)
+    {
+        int[] s = {1,2,3,4,5,6};
+
+        s[3] = 8;
+        System.out.println(s[3]);
+
+
+
+    }
+}
