7d5fdfa313ee14c19e8ab5bf9c3d50265ac5b984
JohnLacklan <janos.f96@gmail.com>
2019-01-21 14:29:48 +0100

    summ all the numbers

---
+++ week2/day1/src/Summm.java
@@ -0,0 +1,25 @@
+public class Summm {
+                        //  Create the usual class wrapper
+                        //  and main method on your own.
+
+                        // - Write a function called `sum` that sum all the numbers
+                        //   until the given parameter and returns with an integer
+
+    public static void main(String[] args)
+    {
+        int a = 58;
+        int b = 88;
+        int c = 8;
+
+
+        System.out.println(Sum(a,b,c));
+
+
+    }
+    public static int Sum (int a, int b, int c ){
+        int summAll = a + b + c ;
+
+        return summAll;
+    }
+
+}
