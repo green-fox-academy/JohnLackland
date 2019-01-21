fd97e94164dd51a4190f784866f3a1b287ff24cd
JohnLacklan <janos.f96@gmail.com>
2019-01-21 14:10:01 +0100

    Greet everyobody

---
+++ week2/day1/src/Greet.java
@@ -0,0 +1,23 @@
+public class Greet {
+    public static void main(String[] args)
+    {
+        String al = "GreenFox";
+
+
+        System.out.println(Helo(al));
+
+
+
+    }
+    public static String Helo( String al )
+    {
+        String greet = "Hello,  " + al + "!";
+
+        return greet;
+
+
+    }
+
+
+
+}
