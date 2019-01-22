2b38486569279d620a03f0f8fa4e1d0b60c07b9e
JohnLacklan <janos.f96@gmail.com>
2019-01-22 12:06:20 +0100

    replace two words

---
+++ week2/day1/src/Unique.java
@@ -0,0 +1,46 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+public class Unique {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] testArray = new int[sc.nextInt()];
+
+        int helpingInt = sc.nextInt();
+
+        for (int i = 0; i < testArray.length; i++) {
+           testArray[i] = helpingInt;
+           helpingInt = sc.nextInt();
+
+        }
+
+        Decres(new)
+
+
+
+        };
+
+        }
+        public static Array Decres(Array testArray[]){
+            int[] newArray = new int[testArray.length];
+
+
+            for (int i = 0; i < testArray.length; i++) {
+
+                for (int j = 0; j < testArray.length; j++) {
+                    if (testArray[i] == )
+                }
+
+                return newArray[];
+            }
+
+
+        }
+
+
+
+
+
+    }
+
+
---
+++ week2/day2/src/SimplReplace.java
@@ -0,0 +1,11 @@
+public class SimplReplace {
+    public static void main(String[] args) {
+        String example = "In a dishwaser far far away";
+
+        example = example.replace("dishwaser","Galaxy");
+        System.out.println(example);
+
+
+        System.out.println(example.replace("dishwaser", "Galaxy" ));
+    }
+}
