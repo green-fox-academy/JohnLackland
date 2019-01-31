e332185bd79f294f8a8a478146346f7ee3496736
JohnLacklan <janos.f96@gmail.com>
2019-01-31 12:51:03 +0100

    Count the ears 2

---
+++ week3/day4/src/RBunnys2.java
@@ -0,0 +1,21 @@
+public class RBunnys2 {
+    public static void main(String[] args) {
+        int bunny = 17;
+
+        System.out.println(earCounter(bunny));
+    }
+
+    public static int earCounter(int bunny) {
+        int b = 0;
+        if (bunny % 2 == 1 && bunny >= 1) {
+           b = earCounter(bunny -1);
+           b += 3;
+
+        } else if (bunny >= 1) {
+            b = earCounter(bunny-1);
+            b += 2;
+        }
+        return b;
+
+    }
+}
