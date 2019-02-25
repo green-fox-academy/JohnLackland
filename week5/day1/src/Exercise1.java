bf359892f84c47522a51e0827295ca9ef4bfd643
JohnLacklan <janos.f96@gmail.com>
2019-02-25 12:22:31 +0100

    Make exercise 3 and add exercise 4

--- week5/day1/src/Exercise1.java
+++ week5/day1/src/Exercise1.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Exercise1 {
     public static void main(String[] args) {
@@ -7,6 +8,7 @@ public class Exercise1 {
 
         List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
 
-        numbers.stream().filter(p -> p % 2 == 0).forEach(System.out::println);
+        List<Integer> evenNumbers = numbers.stream().filter(p -> p % 2 == 0).collect(Collectors.toList());
+        System.out.println(evenNumbers);
     }
 }
--- week5/day1/src/Exercise2.java
+++ week5/day1/src/Exercise2.java
@@ -1,11 +1,15 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Exercise2 {
     public static void main(String[] args) {
         //Write a Stream Expression to get the squared value of the positive numbers from the following list:
 
         List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
-        numbers.stream().filter(number -> number >= 0).map(p -> p * p).forEach(System.out::println);
+        List<Integer> positiveNumbers = numbers.stream()
+                .filter(number -> number >= 0).map(p -> p * p)
+                .collect(Collectors.toList());
+        System.out.println(positiveNumbers);
     }
 }
