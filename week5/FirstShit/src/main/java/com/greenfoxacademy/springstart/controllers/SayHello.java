cb898ad01133355fb533e81331eee98611e525ac
JohnLacklan <janos.f96@gmail.com>
2019-02-26 17:24:02 +0100

    Finish last exercise

---
+++ week5/FirstShit/src/main/java/com/greenfoxacademy/springstart/controllers/SayHello.java
@@ -0,0 +1,25 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SayHello {
+    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+    @RequestMapping("/web/greeting/hello")
+    public String greeting(Model model) {
+        model.addAttribute("color1", (int) (Math.random() * 255));
+        model.addAttribute("color2", (int) (Math.random() * 255));
+        model.addAttribute("color3", (int) (Math.random() * 255));
+        model.addAttribute("oneHello",hellos[(int) (Math.random()*hellos.length-1)]);
+        model.addAttribute("px", (8 + (Math.random()* 999)));
+
+        return "helloToEverybody";
+    }
+}
