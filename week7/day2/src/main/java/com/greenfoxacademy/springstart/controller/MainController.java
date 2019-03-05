14e7fa5acc4f0b4310efdba26c6576084b62ffa8
JohnLacklan <janos.f96@gmail.com>
2019-03-05 16:22:49 +0100

    make a list

--- week7/day2/src/main/java/com/greenfoxacademy/springstart/controller/MainController.java
+++ week7/day2/src/main/java/com/greenfoxacademy/springstart/controller/MainController.java
@@ -6,20 +6,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class MainController {
-    private BankAccount bankAccount = new BankAccount("Simba", 2000.00, "lion");
+    private BankAccount bankAccount = new BankAccount("Simba", 2000.00, "lion",true, true);
     private List<BankAccount> bankAccountList;
 
     public MainController(){
         bankAccountList = Arrays.asList(bankAccount,
-                                        new BankAccount("Pumba", 4000.00, "boar"),
-                                        new BankAccount("Timon", 6660.00, "animal"),
-                                        new BankAccount("Mufase", 4232.00, "halo"));
+                                        new BankAccount("Pumba", 4000.00, "boar",false,false),
+                                        new BankAccount("Timon", 6660.00, "animal",false,true),
+                                        new BankAccount("Mufase", 4232.00, "halo",true,false));
     }
 
     @GetMapping("/show")
@@ -39,4 +40,11 @@ public class MainController {
         model.addAttribute("bankAccountList", bankAccountList);
         return "oneElement";
     }
+
+    @PostMapping("/oneElement")
+    public String addBalance(Model model){
+
+        return "oneElement";
+    }
+
 }
--- week7/day2/src/main/java/com/greenfoxacademy/springstart/model/BankAccount.java
+++ week7/day2/src/main/java/com/greenfoxacademy/springstart/model/BankAccount.java
@@ -4,12 +4,23 @@ public class BankAccount {
     private String name;
     private Double balance;
     private String animaltype;
+    private Boolean isKing;
+    private Boolean badGuy;
 
-    public BankAccount(String name, Double balance, String animaltype){
+    public BankAccount(String name, Double balance, String animalType, Boolean isKing, Boolean badGuy){
         this.name = name;
         this.balance = balance;
-        this.animaltype = animaltype;
+        this.animaltype = animalType;
+        this.isKing = isKing;
+        this.badGuy = badGuy;
+    }
 
+    public void addThem(){
+        if (getKing()){
+            balance+=100;
+        }else {
+            balance+=10;
+        }
     }
 
     public String getName() {
@@ -22,5 +33,13 @@ public class BankAccount {
 
     public String getAnimalType() {
         return animaltype;
+    }
+
+    public Boolean getKing() {
+        return isKing;
+    }
+
+    public Boolean getBadGuy() {
+        return badGuy;
     }
 }
