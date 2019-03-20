3e8eb7742bc656155c978d991a034b1c255bb921
JohnLacklan <janos.f96@gmail.com>
2019-03-20 12:39:14 +0100

    Fix exam exercise and connect it to database

--- week8/weekend/TrialExame/src/main/java/com/greenfoxacademy/weekendtrial/models/Link.java
+++ week8/weekend/TrialExame/src/main/java/com/greenfoxacademy/weekendtrial/models/Link.java
@@ -1,9 +1,21 @@
 package com.greenfoxacademy.weekendtrial.models;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-
+@Entity
 public class URLClass {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    public Long getId() {
+        return id;
+    }
+
     private String url;
     private String alias;
 
